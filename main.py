b13a64a12de8da5fda0d408e89d153ceb4211a61
convinto <convinto@yandex.ru>
2024-12-03 00:26:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(one:= 2*2+2)
+print(two:=2*(2+2))
+print(one==two)
+
+#4th program
+line = '123.456'
+line = float(line)
+print(int(line*10)%10)
